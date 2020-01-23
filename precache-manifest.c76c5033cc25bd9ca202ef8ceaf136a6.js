59637ef00004aa0e5812bc2ae6f7e910dd077e22
yvette <yvette@its6am.com>
2020-01-23 01:08:25 +0100

    Mv the insurance questionnaire build here

---
+++ precache-manifest.c76c5033cc25bd9ca202ef8ceaf136a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a5cfce66a96562009e4d86a9c631556",
+    "url": "/insurance-questionnaire/index.html"
+  },
+  {
+    "revision": "db647cde4057396c0d4d",
+    "url": "/insurance-questionnaire/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "e4e7b40fa6965830ee74",
+    "url": "/insurance-questionnaire/static/css/main.8f3af40f.chunk.css"
+  },
+  {
+    "revision": "db647cde4057396c0d4d",
+    "url": "/insurance-questionnaire/static/js/2.07acb0e2.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/insurance-questionnaire/static/js/2.07acb0e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4e7b40fa6965830ee74",
+    "url": "/insurance-questionnaire/static/js/main.f54d5a68.chunk.js"
+  },
+  {
+    "revision": "b6e4270e767930d0da9e",
+    "url": "/insurance-questionnaire/static/js/runtime-main.d21c13d3.js"
+  }
+]);
